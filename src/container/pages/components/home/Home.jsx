8091550a8e2f8e75f332a2defc2a94e56ed39fec
Kinsammy <fanusamuel@gmail.com>
2024-01-07 18:40:29 +0100

    Login Component in progress...

--- src/container/pages/components/home/Home.jsx
+++ src/container/pages/components/home/Home.jsx
@@ -17,18 +17,12 @@ const Home =()=> {
                 accusamus temporibus quasi? Inventore excepturi est provident laborum deleniti rem rerum nihil quibusdam at debitis.
             </p>
             <div class="social-media">
-                <a href="#"><i class="bx bxl-facebook" ></i></a>
-                <a href="#"><i class="bx bxl-twitter" ></i></a>
-                <a href="#"><i class="bx bxl-instagram-alt" ></i></a>
-                <a href="#"><i class="bx bxl-linkedin" ></i></a>
-                
-                {/* <Link to="/#"><i class="bx bxl-facebook" ></i></Link>
+                <Link to="/#"><i class="bx bxl-facebook" ></i></Link>
                 <Link to="/#"><i class="bx bxl-twitter" ></i></Link>
                 <Link to="/#"><i class="bx bxl-instagram-alt" ></i></Link>
-                <Link to="/#"><i class="bx bxl-linkedin" ></i></Link> */}
+                <Link to="/#"><i class="bx bxl-linkedin" ></i></Link>
             </div>
-            {/* <a href='#'><i className='btn'>Get Started</i></a> */}
-            <Link to="/login">Get Started</Link>
+            <Link className="btn" to="/login">Get Started</Link>
         </div>
         <Professional />
         <div className='home-img'>
