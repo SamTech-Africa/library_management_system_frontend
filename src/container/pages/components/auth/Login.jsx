81a2c5dbfbf5f85eb05a05bdb8b1ab467fc4f732
Kinsammy <fanusamuel@gmail.com>
2024-01-08 11:03:42 +0100

    Login Component in progress...

--- src/container/pages/components/auth/Login.jsx
+++ src/container/pages/components/auth/Login.jsx
@@ -1,51 +1,42 @@
 import React,{useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 
-import '../../styles/auth/Login.css';
+import styles from '../../styles/auth/Login.module.css';
 
 const Login = () => {
-  const [isSticky, setIsSticky] = useState(false);
-  const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsSticky(window.scrollY > 100);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-
-  const handleDarkModeToggle = () => {
-    setDarkMode(!darkMode);
-    document.body.classList.toggle('dark-icon');
-  }
-
-  
-
     
   return (
-    <header className={`header ${isSticky ? 'sticky' : ''}`}>
-     <Link to='/#' className='logo'>SamTech.</Link>
-        <nav className={`navbar ${isSticky ? 'sticky' : ''}`}>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-            <a href='#home' className='active'>Home</a>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-            <a href='#about'>About</a>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-            <a href='#services'>Services</a>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-            <a href='#books'>Books</a>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-            <a href='#contact'>Contact</a>
-      </nav>
-      <div className={`bx ${darkMode ? 'bx-sun' : 'bx-moon'}`} id='darkMode-icon' onClick={handleDarkModeToggle}></div>
+    <div>
+      <header className={styles.header}>
+      <Link to='/#' className='logo'>SamTech.</Link>
+        <nav className={styles.navbar}>
+            <Link to='/' className='active'>Home</Link>
+        </nav>
+      </header>
 
-    </header>
+
+      <section className={styles.formSection}>
+
+        
+        <h2 class="heading">Welcome <span>Back!</span></h2>
+        <p>Login to your Account</p>
+
+        <form className={styles.formInput} action="#">
+            <div class={styles.inputBox}>
+                <input type="text" placeholder="Enter your email" />
+            </div>
+            <div class={styles.inputBox}>
+                <input type="password" placeholder="Enter your password" />
+            </div>
+            <input type="submit" value="Login" className={styles.btn}/>
+        </form>
+        
+        
+
+      </section>
+    </div>
+   
+    
    
   )
 }
