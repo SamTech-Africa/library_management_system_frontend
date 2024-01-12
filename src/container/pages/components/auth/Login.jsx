d030d9f97c7a6567c5883c5b384328e3f729e096
Kinsammy <fanusamuel@gmail.com>
2024-01-12 09:14:36 +0100

    Connceting Frontend to Backend

--- src/container/pages/components/auth/Login.jsx
+++ src/container/pages/components/auth/Login.jsx
@@ -1,9 +1,38 @@
 import React,{useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import styles from '../../styles/auth/Login.module.css';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const data = {
+      email: email,
+      password: password,
+    };
+    
+    try {
+      const response = await axios.get('http://localhost:5252/api/v1/auth/login', data);
+      
+      console.log('Login successfu:', response.data);
+    } catch (error) {
+      console.error('Error during login', error);
+    };
+  }
+
     
   return (
     <div>
@@ -22,12 +51,20 @@ const Login = () => {
         <h2 class="heading">Welcome <span>Back!</span></h2>
         <p>Login to your Account</p>
 
-        <form className={styles.formInput} action="#">
+        <form className={styles.formInput} onSubmit={handleSubmit}>
             <div class={styles.inputBox}>
-                <input type="text" placeholder="Enter your email" />
+                <input
+                 type="text"
+                 placeholder="Enter your email"
+                 value={email}
+                 onChange={handleEmailChange} />
             </div>
             <div class={styles.inputBox}>
-                <input type="password" placeholder="Enter your password" />
+                <input
+                 type="password"
+                 placeholder="Enter your password"
+                 value={password}
+                 onChange={handleChangePassword} />
             </div>
             <div className={styles.others}>
               <div className={styles.rememberMe}>
