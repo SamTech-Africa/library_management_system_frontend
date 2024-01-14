dfde6b47c9adf437f300360ede1b8c986ada61ba
Kinsammy <fanusamuel@gmail.com>
2024-01-14 15:57:08 +0100

    Login Restructuring...

--- src/container/pages/components/auth/Login.jsx
+++ src/container/pages/components/auth/Login.jsx
@@ -54,7 +54,7 @@ const Login = () => {
         </h3>
 
       </div>
-       <section className={styles.formSection}>
+       <div className={styles.formSection}>
         <p>Login to your Account</p>
 
         <form className={styles.formInput} onSubmit={handleSubmit}>
@@ -86,7 +86,7 @@ const Login = () => {
         
         
 
-      </section>
+      </div>
       </div>
     </div>
    
